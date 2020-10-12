68cebf749f4025fdab00b4a0f505247f3a7f8649
Dipesh Bajgain <dipbazz@gmail.com>
2020-10-12 19:25:10 +0545

    Fix spacing and responsive based on code review.
    - Made navbar fixed on top after scroll.
    - footer logo and social icons aligment fixed.
    - fixed the height of subscribe and newsletter section on large screen.
    - provided some spacing to first image of in the magzine.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,15 @@
-window.scroll() = function () { stickyNav(); };
+// for navbar to be sticky on certain offset of page.
+window.onscroll = function () { stickyNav(); };
 
 var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
+// var nav_info = document.getElementById('nav-info');
+var OFFSET_VALUE = 200;
 
 function stickyNav() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('stop');
+  if (window.pageYOffset >= OFFSET_VALUE) {
+    navbar.classList.add('fixed-top');
+    // nav_info.classList.remove('d-lg-block');
   } else {
-    navbar.classListremove('stop');
+    navbar.classList.remove('fixed-top');
   }
-
 }
