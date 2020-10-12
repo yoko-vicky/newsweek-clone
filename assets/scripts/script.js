4cd077b9575f29ff9d6f01cbc3d4bef8355a5f0a
yocosaka <sakayoco55@gmail.com>
2020-10-12 23:32:51 +0900

    Add sticky function to more stories
    Add script and css to fix position of more stories section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,26 @@
 // for navbar to be sticky on certain offset of page.
-window.onscroll = function () { stickyNav(); };
+window.onscroll = function () {
+  stickyNav();
+};
 
 var navbar = document.getElementById("navbar");
-// var nav_info = document.getElementById('nav-info');
+
 var OFFSET_VALUE = 200;
 
 function stickyNav() {
   if (window.pageYOffset >= OFFSET_VALUE) {
     navbar.classList.add('fixed-top');
-    // nav_info.classList.remove('d-lg-block');
   } else {
     navbar.classList.remove('fixed-top');
+  }
+}
+
+var more_stories = document.getElementById('more-stories');
+
+function stickyStories() {
+  if (more_stories.offsetTop >= window.pageYOffset || window.pageYOffset <= offsetBottom) {
+    more_stories.classList.add('position-fixed');
+  } else {
+    more_stories.classList.remove('position-fixed');
   }
 }
